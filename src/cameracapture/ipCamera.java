ef0556a0fcb03911d704135a6d8bf6f87dd87371
yogipratama1 <pyogi6391@gmail.com>
2022-06-22 14:47:53 +0700

    First

---
+++ src/cameracapture/ipCamera.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cameracapture;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class ipCamera {
+    public static String ip;
+    public static String port;
+
+    public static String getIp() {
+        return ip;
+    }
+
+    public static void setIp(String ip) {
+        ipCamera.ip = ip;
+    }
+
+    public static String getPort() {
+        return port;
+    }
+
+    public static void setPort(String port) {
+        ipCamera.port = port;
+    }
+
+}
